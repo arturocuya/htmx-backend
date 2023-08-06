051b3c91a9ba1173d79d2998e34bcd96896fd287
Arturo Cuya <arturocuya099@gmail.com>
2023-08-06 18:23:51 -0500

    morphed into chat

--- main.go
+++ main.go
@@ -4,7 +4,16 @@ import (
 	"text/template"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/utils"
 )
+
+var indexTemplate *template.Template
+var mainInputTemplate *template.Template
+
+func init() {
+	indexTemplate = template.Must(template.ParseFiles("./templates/index.html", "./templates/main-input.html"))
+	mainInputTemplate = template.Must(template.ParseFiles("./templates/main-input.html"))
+}
 
 func main() {
 	app := fiber.New()
@@ -13,21 +22,30 @@ func main() {
 		Name string
 	}
 
+	var messages []string
+
+	app.Use(func(c *fiber.Ctx) error {
+		c.Set("Content-Type", "text/html")
+		return c.Next()
+	})
+
 	app.Get("/", func(context *fiber.Ctx) error {
-		tmpl, err := template.ParseFiles("./templates/index.html")
-
-		if err != nil {
-			return context.SendStatus(500)
-		}
-
 		data := InitialData{Name: "ayumi"}
 
-		err = tmpl.Execute(context.Response().BodyWriter(), data)
+		err := indexTemplate.Execute(context.Response().BodyWriter(), data)
 		if err != nil {
 			return context.SendStatus(500)
 		}
 
-		context.Response().Header.Set("Content-Type", "text/html")
+		return nil
+	})
+
+	app.Post("/send-message", func(context *fiber.Ctx) error {
+		newMessage := utils.CopyString(context.FormValue("message"))
+		messages = append(messages, newMessage)
+
+		tmpl, _ := mainInputTemplate.New("").Parse(`{{range .}}<p>{{.}}</p>{{end}}{{template "main-input" .}}`)
+		tmpl.Execute(context.Response().BodyWriter(), messages)
 
 		return nil
 	})
